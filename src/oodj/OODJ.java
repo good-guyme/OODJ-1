44ca62d0139d89e7fd3fbb2a8e9a09e11b2e2554
Acer <Acer@CAPTAIN>
2024-10-07 12:35:27 +0800

    hamim bro

--- src/oodj/OODJ.java
+++ src/oodj/OODJ.java
@@ -14,6 +14,8 @@ public class OODJ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("hamim");
         // TODO code application logic here
     }
     
