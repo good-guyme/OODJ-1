5a21f4f95169a6e725f4c519157412524ef77420
yihan <yihan@MSI>
2024-11-05 17:16:43 +0800

    5/11/2024

--- src/InventoryManager/Item_Entry.java
+++ src/InventoryManager/Item_Entry.java
@@ -272,7 +272,7 @@ public class Item_Entry extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 810, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
