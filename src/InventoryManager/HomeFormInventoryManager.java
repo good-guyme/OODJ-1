9d7f8cdaf2e9f09b72c6db4c55877f7dde8efa09
yihan <yihan@MSI>
2024-12-11 14:14:58 +0800

    11/12/2024

--- src/InventoryManager/HomeFormInventoryManager.java
+++ src/InventoryManager/HomeFormInventoryManager.java
@@ -5,6 +5,7 @@
 package InventoryManager;
 
 import Login.LoginForm;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 /**
@@ -20,6 +21,8 @@ public class HomeFormInventoryManager extends javax.swing.JFrame {
         initComponents();
         LoginForm lf = new LoginForm();
         usernamelbl.setText(lf.getUsername());
+        ImageIcon img = new ImageIcon(getClass().getResource("InventoryManager/Image/moshayu.jpg"));
+        imgBg.setIcon(img);
     }
 
     /**
@@ -42,7 +45,7 @@ public class HomeFormInventoryManager extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        imgBg = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -90,8 +93,8 @@ public class HomeFormInventoryManager extends javax.swing.JFrame {
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("Item Entry");
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InventoryManager/Image/moshayu.jpg"))); // NOI18N
-        jLabel6.setPreferredSize(new java.awt.Dimension(800, 500));
+        imgBg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InventoryManager/Image/moshayu.jpg"))); // NOI18N
+        imgBg.setPreferredSize(new java.awt.Dimension(800, 500));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -124,7 +127,7 @@ public class HomeFormInventoryManager extends javax.swing.JFrame {
                     .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(82, 82, 82))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 800, Short.MAX_VALUE))
+                .addComponent(imgBg, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 800, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -156,7 +159,7 @@ public class HomeFormInventoryManager extends javax.swing.JFrame {
                                 .addComponent(jLabel4)))))
                 .addGap(103, 103, 103))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))
+                .addComponent(imgBg, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -228,13 +231,13 @@ public class HomeFormInventoryManager extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel imgBg;
     private javax.swing.JButton itementrybtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JButton logoutbtn;
