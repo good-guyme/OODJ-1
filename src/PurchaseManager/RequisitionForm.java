4c401ea820400b2a7c1ab97a0f24b479a110b3f1
Potatoes66 <gordenwongmf123@gmail.com>
2024-12-15 20:40:09 +0800

    15/12/2024

--- src/PurchaseManager/RequisitionForm.java
+++ src/PurchaseManager/RequisitionForm.java
@@ -29,6 +29,7 @@ public class RequisitionForm extends javax.swing.JFrame {
     public RequisitionForm() {
         initComponents();
         usernametxt1.setText(lf.getUsername());
+        loadDataIntoTable();
 
     }
 
@@ -294,10 +295,10 @@ public class RequisitionForm extends javax.swing.JFrame {
                     .addComponent(amounttxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel6)
                     .addComponent(datetxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(11, 11, 11)
+                .addGap(9, 9, 9)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
                     .addComponent(usernametxt2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
